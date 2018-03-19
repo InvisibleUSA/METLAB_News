62047b335a1694b01947a9819ac0887599d8f0d6
MarcoRempfer <37103918+MarcoRempfer@users.noreply.github.com>
2018-03-19 14:38:15 +0100

    Recovered CodeStyle

--- Software/src/main/java/MetlabNews/MainUI.java
+++ Software/src/main/java/MetlabNews/MainUI.java
@@ -12,36 +12,40 @@ import com.vaadin.ui.TextField;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
 
+
+
 /**
- * This UI is the application entry point. A UI may either represent a browser window 
+ * This UI is the application entry point. A UI may either represent a browser window
  * (or tab) or some part of an HTML page where a Vaadin application is embedded.
  * <p>
- * The UI is initialized using {@link #init(VaadinRequest)}. This method is intended to be 
+ * The UI is initialized using {@link #init(VaadinRequest)}. This method is intended to be
  * overridden to add component to the user interface and initialize non-component functionality.
  */
 @Theme("mytheme")
-public class MainUI extends UI {
+public class MainUI extends UI
+{
+	@Override
+	protected void init(VaadinRequest vaadinRequest)
+	{
+		final VerticalLayout layout = new VerticalLayout();
 
-    @Override
-    protected void init(VaadinRequest vaadinRequest) {
-        final VerticalLayout layout = new VerticalLayout();
-        
-        final TextField name = new TextField();
-        name.setCaption("Type your name here:");
+		final TextField name = new TextField();
+		name.setCaption("Type your name here:");
 
-        Button button = new Button("Click Me");
-        button.addClickListener(e -> {
-            layout.addComponent(new Label("Thanks " + name.getValue() 
-                    + ", it works!"));
-        });
-        
-        layout.addComponents(name, button);
-        
-        setContent(layout);
-    }
+		Button button = new Button("Click Me");
+		button.addClickListener(e -> {
+			layout.addComponent(new Label("Thanks " + name.getValue()
+					                              + ", it works!"));
+		});
 
-    @WebServlet(urlPatterns = "/*", name = "MyUIServlet", asyncSupported = true)
-    @VaadinServletConfiguration(ui = MainUI.class, productionMode = false)
-    public static class MyUIServlet extends VaadinServlet {
-    }
+		layout.addComponents(name, button);
+
+		setContent(layout);
+	}
+
+	@WebServlet(urlPatterns = "/*", name = "MyUIServlet", asyncSupported = true)
+	@VaadinServletConfiguration(ui = MainUI.class, productionMode = false)
+	public static class MyUIServlet extends VaadinServlet
+	{
+	}
 }
