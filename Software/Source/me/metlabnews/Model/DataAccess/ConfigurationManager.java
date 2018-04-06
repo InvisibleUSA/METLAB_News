d0cad10d057e7854e110deeb5418bcb3350d5a9a
Toberto91 <toberto.91@gmail.com>
2018-04-06 18:59:33 +0200

    - ConfigurationManager updatet

--- Software/Source/me/metlabnews/Model/DataAccess/ConfigurationManager.java
+++ Software/Source/me/metlabnews/Model/DataAccess/ConfigurationManager.java
@@ -28,4 +28,79 @@ public class ConfigurationManager
 		}
 		return ConfigurationManager.instance;
 	}
+
+
+	private enum TypePrefered
+	{
+		RSSFeed,
+		Website
+	}
+
+
+	// Benny
+	public long getCrawlerTimeout()
+	{
+		return 0;
+	}
+
+	public int getMaxDocsPerDomain()
+	{
+		return 0;
+	}
+
+	public TypePrefered getTypePrefered()
+	{
+		return TypePrefered.RSSFeed;
+	}
+
+
+	// Erik
+	public String getBaseXLoginUsername()
+	{
+		return "";
+	}
+
+	public String getBaseXLoginPassword()
+	{
+		return "";
+	}
+
+	public String getBaseXPath()
+	{
+		return "";
+	}
+
+	public String getSQLLoginUsername()
+	{
+		return "";
+	}
+
+	public String getSQLLoginPassword()
+	{
+		return "";
+	}
+
+	public String getMailFromAddress()
+	{
+		return "";
+	}
+
+	public String getMailLoginPassword()
+	{
+		return "";
+	}
+
+	public String getMailSMTPServer()
+	{
+		return "";
+	}
+
+
+	// Tobias
+	public String getLoggerLogFilePath()
+	{
+		return "";
+	}
+
+
 }
