40a9180a0a76322878aff19f2722e8f634dc8ae6
InvisibleUSA <lukas@niedergrise.net>
2018-04-12 20:45:31 +0200

    Fixed SQL issues
    Signed-off-by: InvisibleUSA <lukas@niedergrise.net>

--- Software/Source/me/metlabnews/Model/DataAccess/Queries/QueryAddUser.java
+++ Software/Source/me/metlabnews/Model/DataAccess/Queries/QueryAddUser.java
@@ -24,13 +24,11 @@ public class QueryAddUser extends QueryBase
 	@Override
 	protected String createSQLQuery()
 	{
-
 		return "INSERT INTO Abonnent (EMail, Name, VName, PW, Firma, isAdmin, isVerified) VALUES ('" + email + "','" + lastName + "','" + firstName + "','" + password + "','" + organisationName + "', 0, 0)";
 	}
 
 	@Override
 	protected void processResults(ResultSet rs, String str)
 	{
-
 	}
 }
