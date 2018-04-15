42e6b74c119ce761b7af4b8b03b56768c3d9219a
MarcoRempfer <37103918+MarcoRempfer@users.noreply.github.com>
2018-04-15 11:45:23 +0200

    Added workaround for weird MariaDB fuckup in RelationalQueriesTest
    Also renamed file

--- Software/Test/me/metlabnews/Model/DataAccess/Queries/RelationalQueriesTest.java
+++ Software/Test/me/metlabnews/Model/DataAccess/Queries/RelationalQueriesTest.java
@@ -4,6 +4,7 @@ import me.metlabnews.Model.DataAccess.ConfigurationManager;
 import me.metlabnews.Model.DataAccess.DbConnectors.RelationalDbConnector;
 import me.metlabnews.Model.Entities.Organisation;
 import me.metlabnews.Model.Entities.Subscriber;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
@@ -11,17 +12,39 @@ import static org.junit.jupiter.api.Assertions.*;
 
 
 
-class RelationalQueryTest
+class RelationalQueriesTest
 {
 	@BeforeAll
 	public static void initialize()
 	{
 		ConfigurationManager.getInstance().initialize();
 		RelationalDbConnector.getInstance().initialize();
-		m_Organisation = new Organisation("Test");
-		m_Organisation.setId(1);
+		m_Organisation = new Organisation(m_organisationName);
+		assertTrue(new AddOrganisationQuery(m_Organisation).execute());
 	}
 
+	@AfterAll
+	public static void close()
+	{
+		try
+		{
+			new RemoveOrganisationQuery(m_Organisation).execute();
+		}
+		catch(Exception e)
+		{
+			// This can happen in two different scenarios:
+			// 1) There is an actual bug in AddOrganisationQuery or in RemoveOrganisationQuery
+			// 2) There occurs a mysteriously fucked up behaviour in MariaDB itself
+			System.err.println("[MAY OR MAY NOT BE AN ERROR] " + e.toString());
+		}
+	}
+
+
+	@Test
+	public void getOrganisation()
+	{
+		assertTrue(new GetOrganisationQuery(m_organisationName).execute());
+	}
 
 	@Test
 	public void addAndRemoveSubscriber()
@@ -39,14 +62,8 @@ class RelationalQueryTest
 		assertTrue(new GetSystemAdministratorQuery("sys.admin@test.de").execute());
 	}
 
-	@Test
-	public void addAndRemoveOrganisation()
-	{
-		Organisation organisation = new Organisation("TempOrg");
-		assertTrue(new AddOrganisationQuery(organisation).execute());
-		assertTrue(new RemoveOrganisationQuery(organisation).execute());
-	}
 
 
 	private static Organisation m_Organisation;
+	private final static String m_organisationName = "TestOrg";
 }
