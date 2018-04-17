d99eb4ab3cbdec1c98bbae092634b07ec36c5b3c
InvisibleUSA <lukas.niedergriese1@gmail.com>
2018-04-17 09:10:49 +0200

    Added bugs, removed features
    Signed-off-by: InvisibleUSA <lukas.niedergriese1@gmail.com>

--- Software/Source/me/metlabnews/Model/BusinessLogic/UserManager.java
+++ Software/Source/me/metlabnews/Model/BusinessLogic/UserManager.java
@@ -1,15 +1,17 @@
 package me.metlabnews.Model.BusinessLogic;
 
-import com.vaadin.data.validator.EmailValidator;
 import me.metlabnews.Model.Common.Logger;
+import me.metlabnews.Model.DataAccess.ConfigurationManager;
 import me.metlabnews.Model.DataAccess.Queries.*;
 import me.metlabnews.Model.Entities.Subscriber;
+import me.metlabnews.Model.ResourceManagement.IResource;
 import me.metlabnews.Presentation.IUserInterface;
 import me.metlabnews.Presentation.Messages;
 import me.metlabnews.Presentation.Session;
 import me.metlabnews.Presentation.IUserInterface.IGenericEvent;
 import me.metlabnews.Presentation.IUserInterface.IGenericFailureEvent;
 import me.metlabnews.Presentation.IUserInterface.IGetStringArrayEvent;
+import org.apache.commons.validator.routines.EmailValidator;
 
 
 
@@ -194,5 +196,92 @@ public class UserManager
 	}
 
 	// endregion Client Admin Interaction
+
+	public static class Validator implements IResource
+	{
+		public static Validator getInstance()
+		{
+			if(m_instance == null)
+			{
+				m_instance = new Validator();
+			}
+			return m_instance;
+		}
+
+
+		@Override
+		public void initialize()
+		{
+			ConfigurationManager config = ConfigurationManager.getInstance();
+			m_minimumPasswordLength = config.getSecurityPasswordMinimumLength();
+			m_lowerCaseLetterIsRequired = config.getSecurityPasswordLowerCaseLetterRequired();
+			m_upperCaseLetterIsRequired = config.getSecurityPasswordUpperCaseLetterRequired();
+			m_digitIsRequired = config.getSecurityPasswordDigitRequired();
+			m_specialCharacterIsRequired = config.getSecurityPasswordSpecialCharacterRequired();
+			m_whitespaceIsForbidden = config.getSecurityPasswordWhitespaceForbidden();
+		}
+
+		@Override
+		public void close()
+		{
+
+		}
+
+
+		@SuppressWarnings("BooleanMethodIsAlwaysInverted")
+		private static boolean validatePassword(String password)
+		{
+			boolean valid;
+			if(password.length() < m_minimumPasswordLength)
+			{
+				valid = false;
+			}
+			else
+			{
+				StringBuilder regex = new StringBuilder("([^.]+?)");
+				if(m_digitIsRequired)
+				{
+					regex.append("(?=.*[0-9])");
+				}
+				if(m_lowerCaseLetterIsRequired)
+				{
+					regex.append("(?=.*[a-z])");
+				}
+				if(m_upperCaseLetterIsRequired)
+				{
+					regex.append("(?=.*[A-Z])");
+				}
+				if(m_specialCharacterIsRequired)
+				{
+					regex.append("(?=.*[@#$%^&+=])");
+				}
+				if(m_whitespaceIsForbidden)
+				{
+					regex.append("(?=\\S+$)");
+				}
+				valid = password.matches(regex.toString());
+			}
+			return valid;
+		}
+
+		@SuppressWarnings("BooleanMethodIsAlwaysInverted")
+		private static boolean validateEmailAddress(String address)
+		{
+			return EmailValidator.getInstance().isValid(address);
+		}
+
+
+
+		private static Validator m_instance = null;
+
+		// region password requirements
+		private static int     m_minimumPasswordLength;
+		private static boolean m_digitIsRequired;
+		private static boolean m_lowerCaseLetterIsRequired;
+		private static boolean m_upperCaseLetterIsRequired;
+		private static boolean m_specialCharacterIsRequired;
+		private static boolean m_whitespaceIsForbidden;
+		// endregion password requirements
+	}
 
 }
